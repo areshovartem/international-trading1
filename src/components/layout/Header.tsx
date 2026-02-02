9338a92f1aeb845f0c4dbba0d0bf0cea83531653
Artem <artem@MacBook-Pro--Artem.local>
2026-02-02 23:21:17 +0300

    progress bar

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -28,6 +28,8 @@ export default function Header() {
     return () => ro.disconnect()
   }, [])
 
+
+  {/* deploy refresh */}
   // прогресс скролла
   useEffect(() => {
     const onScroll = () => {
